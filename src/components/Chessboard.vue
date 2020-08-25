f95aa21f32df69a3bf8e1865cdb6945f4c07d271
Obada <obada.khalili@outlook.com>
2020-08-25 16:46:19 +0300

    Set up chess-board library

--- src/components/Chessboard.vue
+++ src/components/Chessboard.vue
@@ -1,10 +1,16 @@
 <template>
-  
+  <aside>
+    <chess-board draggable-pieces drop-off-board="snapback"></chess-board>
+  </aside>
 </template>
 
 <script>
 export default {
   name: "Chessboard",
+  mounted() {
+    const board = document.querySelector("chess-board")
+    board.position = { c6: "bN" }
+  },
   methods: {
     clearBoard() {
       this.$emit("change-button-message", "Take a Tour")
@@ -13,3 +19,18 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+aside {
+  width: 550px;
+  margin: auto;
+  padding: 3rem 0 0 0;
+}
+
+@media only screen and (max-width: 816px) {
+  aside {
+    width: 95%;
+    padding: 0;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue"
 import router from "./router"
+import "chessboard-element"
 import App from "./App"
 
 Vue.config.productionTip = false
